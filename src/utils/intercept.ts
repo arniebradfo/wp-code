6fb0034e503d532285956c547dc12286f2778163
James Bradford <j.bradford@pnnl.gov>
2020-03-01 22:15:37 -0800

    cleanup intercept update

--- src/utils/intercept.ts
+++ src/utils/intercept.ts
@@ -1,17 +1,7 @@
-import { Quicktags, SwitchEditors, Wp, WpEditorInitialize } from "../../index";
-
-declare let quicktags: Quicktags;
-declare let switchEditors: SwitchEditors;
-declare const commentReply;
-declare const wp: Wp;
-
-// TODO: create a custom event for these?
-// https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events
-// ...and/or create a wrapper function that intercepts any function? - run it a multiple intervals?s
-// https://blakeembrey.com/articles/2014/01/wrapping-javascript-functions/
-
-
 export function interceptFun<T extends Function>(fun: T, runBefore: T | null = null, runAfter: T | null = null, cancel = false) {
+    // TODO: create a custom event for these?
+    // https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events
+
     // https://blakeembrey.com/articles/2014/01/wrapping-javascript-functions/
     // https://stackoverflow.com/a/326693/5648839
     // wish we could just use Typescript's @decorators onFunctions() but we cant, only classes :(
@@ -31,71 +21,43 @@ export function interceptFun<T extends Function>(fun: T, runBefore: T | null = n
     }
 }
 
+// TODO: interceptFun on this too?
+// declare let switchEditors: SwitchEditors;
+// export function interceptSwitchEditors() {
 
-/// DELETE BELOW ///
+//     try {
 
-export function interceptQuickTags(hijackQuickTagsCallback: Quicktags, ) {
-    quicktags = hijackQuickTagsCallback
-}
+//         var oldGo = switchEditors.go
+//         switchEditors.go = (id, mode) => {
+//             console.log(id, mode);
+//             oldGo(id, mode);
+//         }
 
-export function interceptEditor(editorInitCallback: WpEditorInitialize) {
-    // this doesn't seem to run soon enough...
-    const oldWpEditorInitialize = wp.editor.initialize
-    wp.editor.initialize = (id, settings) => {
-        editorInitCallback(id, settings)
-        oldWpEditorInitialize(id, settings)
-    }
-}
+//         // this hijacks the SwitchEditors.switchEditors() click event setup in SwitchEditors.init()
+//         const oldSwitchEditorTabClass = 'wp-switch-editor'
+//         const newSwitchEditorTabClass = oldSwitchEditorTabClass + '-intercepted'
+//         let switchEditorTabs = document.querySelectorAll('.' + oldSwitchEditorTabClass)
 
-export function interceptCommentReply(addcomment: (post_id: number) => void, send: () => void) {
-    const oldAddComment = commentReply.addcomment
-    commentReply.addcomment = (post_id) => {
-        addcomment(post_id)
-        oldAddComment(post_id)
-    }
+//         for (let i = 0; i < switchEditorTabs.length; i++) {
+//             const tab = switchEditorTabs[i];
+//             tab.classList.replace(oldSwitchEditorTabClass, newSwitchEditorTabClass)
+//         }
 
-    const oldSend = commentReply.send
-    commentReply.send = () => {
-        send()
-        oldSend()
-    }
-}
+//         document.addEventListener('click', (event) => {
+//             var id, mode,
+//                 target = event.target as HTMLElement
 
-export function interceptSwitchEditors() {
+//             if (target.classList.contains(newSwitchEditorTabClass)) {
+//                 id = target.dataset.wpEditorId // .attr('data-wp-editor-id');
+//                 mode = target.classList.contains('switch-tmce') ? 'tmce' : 'html';
+//                 switchEditors.go(id, mode);
+//             }
+//         });
 
-    try {
+//     } catch (error) {
 
-        var oldGo = switchEditors.go
-        switchEditors.go = (id, mode) => {
-            console.log(id, mode);
-            oldGo(id, mode);
-        }
+//     }
 
-        // this hijacks the SwitchEditors.switchEditors() click event setup in SwitchEditors.init()
-        const oldSwitchEditorTabClass = 'wp-switch-editor'
-        const newSwitchEditorTabClass = oldSwitchEditorTabClass + '-intercepted'
-        let switchEditorTabs = document.querySelectorAll('.' + oldSwitchEditorTabClass)
-
-        for (let i = 0; i < switchEditorTabs.length; i++) {
-            const tab = switchEditorTabs[i];
-            tab.classList.replace(oldSwitchEditorTabClass, newSwitchEditorTabClass)
-        }
-
-        document.addEventListener('click', (event) => {
-            var id, mode,
-                target = event.target as HTMLElement
-
-            if (target.classList.contains(newSwitchEditorTabClass)) {
-                id = target.dataset.wpEditorId // .attr('data-wp-editor-id');
-                mode = target.classList.contains('switch-tmce') ? 'tmce' : 'html';
-                switchEditors.go(id, mode);
-            }
-        });
-
-    } catch (error) {
-
-    }
-
-}
+// }
 
 
--- src/wp-code.ts
+++ src/wp-code.ts
@@ -2,7 +2,7 @@ import PostEditor from "./editor-types/post-editor";
 import SimpleEditor from "./editor-types/simple-editor";
 import wpHtmlMonarchLanguage from "./langs/wp-html.lang";
 import './wp-code.css';
-import { interceptSwitchEditors, interceptQuickTags, interceptEditor, interceptFun } from "./utils/intercept";
+import { interceptFun } from "./utils/intercept";
 import DiscussionEditor from "./editor-types/discussion-editor";
 import { SwitchEditors, Quicktags, Wp } from "../index";
 
@@ -14,7 +14,7 @@ declare const wp: Wp;
 console.log('loaded wp-code plugin')
 
 const wpCode: { // global
-    instances: (PostEditor | SimpleEditor)[]
+    instances: (SimpleEditor)[]// TODO: make IWpEditor interface for here
 } = {
     instances: []
 }
@@ -27,39 +27,20 @@ const editorTypeMap = {
     'replycontent': DiscussionEditor
 }
 
-
-const hijackQuickTagsCallback = settings => {
+const hijackQuickTags = settings => {
     console.log('hijacked quicktags and ran wp-code, settings:\n', settings);
     const textarea = document.getElementById(settings.id) as HTMLTextAreaElement;
-    // const editorType = settings.id === 'content' ? PostEditor : SimpleEditor;
     const editorType = editorTypeMap.hasOwnProperty(settings.id) ? editorTypeMap[settings.id] : SimpleEditor;
     wpCode.instances.push(new editorType(textarea, settings.id))
 };
 
-quicktags = interceptFun<(settings) => void>(quicktags, hijackQuickTagsCallback, null, true)
+quicktags = interceptFun<(settings) => void>(quicktags, hijackQuickTags, null, true)
 
-// this doesn't seem to run soon enough...
-wp.editor.initialize = interceptFun<(id, settings) => void>(wp.editor.initialize, (id, settings) => console.log(id, settings));
-
+// TODO: setup comment switching, possibly in DiscussionEditor
 commentReply.addcomment = interceptFun<(post_id) => void>(commentReply.addcomment, (post_id) => console.log('commentReply.addcomment intercepted', post_id));
 commentReply.send = interceptFun<() => void>(commentReply.send, () => console.log('commentReply.send intercepted'));
 
+// TODO: intercept switch editors
 
-// interceptQuickTags(settings => {
-//     console.log('hijacked quicktags and ran wp-code, settings:\n', settings);
-//     const textarea = document.getElementById(settings.id) as HTMLTextAreaElement;
-//     // const editorType = settings.id === 'content' ? PostEditor : SimpleEditor;
-//     const editorType = editorTypeMap.hasOwnProperty(settings.id) ? editorTypeMap[settings.id] : SimpleEditor;
-//     wpCode.instances.push(new editorType(textarea, settings.id))
-// });
-
-// interceptSwitchEditors();
-// interceptEditor((id, settings) => console.log('hijacked editor ', '\nid:\n', id, '\nsettings:\n', settings)
-
-// Really wish this worked??
-// declare const jQuery;
-// const $ = jQuery;
-// console.log($(document).on('wp-before-tinymce-init', (event, init) => console.log(event, init)))
-// console.log($(document).on('wp-before-quicktags-init', (event, init) => console.log(event, init)))
-// console.log($(document).one('wp-before-tinymce-init.text-widget-init', (event, init) => console.log(event, init)))
-// $(document).trigger('wp-before-tinymce-init') // ???
+// TODO: this doesn't seem to run soon enough...
+// wp.editor.initialize = interceptFun<(id, settings) => void>(wp.editor.initialize, (id, settings) => console.log(id, settings));
